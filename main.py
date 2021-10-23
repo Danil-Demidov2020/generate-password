a935b678bfa4ef131853abc813e2768025c691b1
Данил Демидов <72912815+Danil-Demidov2020@users.noreply.github.com>
2021-10-23 14:30:06 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import random
 
 # Набор доступных символов.
-ARRAY_SYMBOLS = [
-    '0', '1', '2', '3', '4',
-    '5', '6', '7', '8', '9',
-    'a', 'b', 'c',
-    'A', 'B', 'C',
-    '!', '@']
+ARRAY_SYMBOLS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z',
+                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                 'U', 'V', 'W', 'X', 'Y', 'Z',
+                 '!', '@', '#', '$', '&', '?', '-', '+', '=', '~',
+                 '_', '^', '*', '.']
 
 # Получаем количество символов в пароле.
 COUNT_SYMBOLS = 4
-input_count = input('Введите количество символов в пароле: ')
+input_count = input("Введите количество символов в пароле: ")
 if input_count.isdigit():
     count_symbols = int(input_count)
 else:
@@ -27,18 +28,17 @@ def random_symbols():
     ]
 
 
-print(f'Количество доступных сиволов: {len(ARRAY_SYMBOLS)}')
-print(f'Доступные сиволы: {(ARRAY_SYMBOLS)}')
+print(f'Количество доступных символов: {len(ARRAY_SYMBOLS)}')
+print(f'Доступные символы: {ARRAY_SYMBOLS}')
 print(f'Количество возможных комбинаций: {count_variant}')
 
+# Генерируем уникальный пароль.
 password = ''
-
-# Генирируем уникальный пароль.
 for i in range(0, count_symbols):
     password = password + f'{random_symbols()}'
 
 print(f'Сгенерированный пароль: {password}')
 
 # Запись пароля в файл.
-with open('password.txt', 'a') as password_string:
+with open(f'passwords.txt', 'a') as password_string:
     password_string.write('{}\n'.format(f'{password}'))
